cc1340fca4e8ce43819c94e8d47a7ec5047c6648
Romar Nicholas G. Mandap <romnic.mandap@gmail.com>
2026-06-14 00:26:05 +0800

    fix time bug dateTimeConverter

--- src/util/helperfunctions.js
+++ src/util/helperfunctions.js
@@ -88,7 +88,7 @@ export function dateTimeConverter(dateObj) {
     {/* returns ["yyyy-mm-dd", "hh:mm"] */ }
     let h = dateObj.getHours() < 10 ? "0" + dateObj.getHours() : dateObj.getHours()
     let m = dateObj.getMinutes() < 10 ? "0" + dateObj.getMinutes() : dateObj.getMinutes()
-    return [dateObj.toISOString().split("T")[0], h + ":" + m]
+    return [dateObj.toLocaleString("sv").split(" ")[0], h + ":" + m]
 }
 
 export function dayOfWeek(date) {
